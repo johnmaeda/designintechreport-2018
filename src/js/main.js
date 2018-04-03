e27d2bde9f44f96da53940d0a2f258908c716ab3
Shota Matsuda <shota@takram.com>
2018-04-03 19:56:02 +0900

    added a basic language selection functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,46 @@
+import Cookies from 'js-cookie'
+
 import Slideshow from './module/Slideshow'
 
 import '../css/main.scss'
 
 const slideshow = new Slideshow()
 
-async function main() {
+let selectedLanguage = null
+
+async function selectLanguage(language) {
+  if (language === selectedLanguage) {
+    return
+  }
   let source
   if (process.env.NODE_ENV !== 'production') {
-    ({ default: source } = await import('../../markdown/english.md'))
+    ({ default: source } = await import(`../../markdown/${language}.md`))
   } else {
-    source = await window.fetch('markdown/english.md').then(response => {
+    source = await window.fetch(`markdown/${language}.md`).then(response => {
       return response.text()
     })
   }
-  slideshow.init(source)
+  slideshow.load(source)
+  Cookies.set('language', language)
 }
 
-main().catch(error => {
-  console.log(error)
-})
+function main() {
+  let language = Cookies.get('language')
+  if (!language) {
+    const [tag] = window.navigator.language.split('-')
+    switch (tag) {
+      case 'ja':
+        language = 'japanese'
+        break
+      default:
+        language = 'english'
+        break
+    }
+  }
+  selectLanguage(language)
+}
+
+main()
 
 if (module.hot) {
   module.hot.accept('../../markdown/english.md', async () => {
