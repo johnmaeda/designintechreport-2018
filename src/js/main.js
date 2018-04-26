642ee7cd375ecdde21db1ad91bf3bdddf5cf651e
Shota Matsuda <shota@takram.com>
2018-04-26 12:34:19 +0900

    added dynamic language selection

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,39 @@
 /* global Typekit */
 
+import 'cross-fetch/polyfill'
 import AcceptLanguage from 'accept-language'
-import Cookies from 'js-cookie'
-
 import loadScript from './module/loadScript'
+import queryString from 'query-string'
 import Slideshow from './module/Slideshow'
 
 import '../css/main.scss'
 
-const slideshow = new Slideshow()
+export const supportedLanguages = {
+  'en': 'English',
+  'ja': 'Japanese'
+}
 
 const acceptLanguage = AcceptLanguage.create()
-acceptLanguage.languages(['en-US', 'ja-JP'])
-let selectedLanguage = null
+acceptLanguage.languages(Object.keys(supportedLanguages))
 
-async function selectLanguage (language, force = false) {
-  if (language === selectedLanguage && !force) {
-    return
-  }
+let selectedLanguage
+
+const slideshow = new Slideshow()
+
+async function loadSource (language) {
   let source
   if (process.env.NODE_ENV !== 'production') {
     // We need to supply string literals in import() to make the hot module
     // replacement able to track their changes.
     switch (language) {
-      case 'en-US':
-        ({ default: source } = await import(`../../markdown/en-US.md`))
+      case 'en':
+        ({ default: source } = await import('../../markdown/en.md'))
         break
-      case 'ja-JP':
-        ({ default: source } = await import(`../../markdown/ja-JP.md`))
+      case 'ja':
+        ({ default: source } = await import('../../markdown/ja.md'))
         break
       default:
-        throw new Error()
+        throw new Error(`Unsupported language: ${language}`)
     }
   } else {
     source = await window.fetch(`markdown/${language}.md`).then(response => {
@@ -38,14 +41,21 @@ async function selectLanguage (language, force = false) {
     })
   }
   slideshow.load(source)
-  document.documentElement.setAttribute('lang', language)
-  Cookies.set('language', language)
+}
+
+export async function selectLanguage (locale) {
+  const language = acceptLanguage.get(locale)
+  if (language === selectedLanguage) {
+    return
+  }
+  await loadSource(language)
   selectedLanguage = language
+  document.documentElement.setAttribute('lang', language)
 }
 
 function main () {
-  const language = Cookies.get('language') || window.navigator.language
-  selectLanguage(acceptLanguage.get(language))
+  const query = queryString.parse(window.location.search)
+  selectLanguage(query.lang || window.navigator.language)
 }
 
 main()
@@ -58,9 +68,9 @@ loadScript('https://use.typekit.net/aos0mpl.js').then(() => {
 
 if (module.hot) {
   const callback = () => {
-    selectLanguage(selectedLanguage, true)
+    loadSource(selectedLanguage)
   }
-  // Same discussion above.
-  module.hot.accept('../../markdown/en-US.md', callback)
-  module.hot.accept('../../markdown/ja-JP.md', callback)
+  // Same discussion above
+  module.hot.accept('../../markdown/en.md', callback)
+  module.hot.accept('../../markdown/ja.md', callback)
 }
--- src/js/module/Slideshow.js
+++ src/js/module/Slideshow.js
@@ -2,12 +2,15 @@
 
 import 'remark/src/remark'
 
+import { supportedLanguages, selectLanguage } from '../main'
 import SlideObserver from './SlideObserver'
 
 export default class Slideshow {
   constructor (options = {}) {
     this.options = options
     this.slides = new WeakMap()
+    this.languageSelectors = null
+    this.onSelectLanguage = this.onSelectLanguage.bind(this)
   }
 
   async load (source) {
@@ -33,6 +36,7 @@ export default class Slideshow {
           },
           ...this.options
         })
+        this.addLanguageSelector()
       } else {
         this.remark.loadFromString(source)
       }
@@ -48,5 +52,31 @@ export default class Slideshow {
         this.slides.set(element, new SlideObserver(element, index + 1))
       }
     })
+  }
+
+  createLanguageSelectors () {
+    return Object.entries(supportedLanguages).map(([language, name]) => {
+      const item = document.createElement('div')
+      item.innerHTML = name
+      item.setAttribute('data-lang', language)
+      item.addEventListener('click', this.onSelectLanguage)
+      return item
+    })
+  }
+
+  addLanguageSelector () {
+    const container = document.createElement('div')
+    container.setAttribute('class', 'language-selector')
+    this.languageSelectors = this.createLanguageSelectors()
+    this.languageSelectors.forEach(selector => {
+      container.appendChild(selector)
+    })
+    const remarkContainer = document.querySelector('.remark-slides-area')
+    remarkContainer.appendChild(container)
+  }
+
+  onSelectLanguage (event) {
+    const language = event.target.getAttribute('data-lang')
+    selectLanguage(language)
   }
 }
